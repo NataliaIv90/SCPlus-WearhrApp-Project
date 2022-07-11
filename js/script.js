c4f852196fa479197ea59dab9ccb5097ea3e3937
NataliaIv90 <ivantsovana@gmail.com>
2022-07-11 09:34:34 +0300

    Make code cleaner
    Changed design of current weather section, deleted old script code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-// Implement the 2 following features, plus the bonus feature if you can 💪:
-
-// ⏰Feature #1
-// In your project, display the current date and time using JavaScript: Tuesday 16:00
-
-//formatting date parameters if they<10 (ex "4" => "04")
+const apiKey = "bd6a33bf1ae4ff07cbfe080ca9b11f66";
 
 function formatDate(value) {
   if (value < 10) {
@@ -12,65 +7,9 @@ function formatDate(value) {
   }
 }
 
-//function for updating innerHTML
 function updateValue(inputID, inputValue) {
   let input = document.querySelector(inputID);
   input.innerHTML = inputValue;
-}
-
-function changeTemperature(responce) {
-  let location = responce.data.name;
-  updateValue("#cityName", location);
-
-  let currentTemperature = Math.round(responce.data.main.temp);
-  currentTemperature = `${currentTemperature}`;
-  updateValue("#temperature", currentTemperature);
-
-  let currentHymidity = `${responce.data.main.humidity}%`;
-  updateValue("#hymidity", currentHymidity);
-
-  let currentWind = Math.round(responce.data.wind.speed);
-  currentWind = `${currentWind}km/h`;
-  updateValue("#wind", currentWind);
-
-  let currentClouds = `${responce.data.clouds.all}%`;
-  updateValue("#clouds", currentClouds);
-
-  // const weatherDescriptionIcon = {
-  //   "clear sky": "clearSky.svg",
-  //   "few clouds": "clearSky.svg",
-  //   "scattered clouds": "sunAndClouds.svg",
-  //   "broken clouds": "sunAndClouds.svg",
-  //   "overcast clouds": "clouds.svg",
-  //   "shower rain": "showerRain.svg",
-  //   "light rain": "showerRain.svg",
-  //   rain: "rain.svg",
-  //   thunderstorm: "thunderstorm.svg",
-  //   snow: "snow.svg",
-  //   mist: "mist.svg",
-  //   "overcast clouds": "clouds.svg",
-  // };
-
-  // let weatherIconKey = responce.data.weather[0].description;
-  // let weatherIcon = `<img
-  //     src="images/${weatherDescriptionIcon[weatherIconKey]}"
-  //     id="weather-icon"
-  //     alt="${weatherIconKey}"
-  //   />`;
-  // updateValue("#current-weather-icon", weatherIcon);
-
-  // weatherIcon = `<img src="images/${weatherIcon}" id="weather-icon" alt="rainy weather" />`;
-  // updateValue("#current-weather-icon", weatherIcon);
-
-  let weatherIconDescription = responce.data.weather[0].description;
-  let weatherIcon = responce.data.weather[0].icon;
-
-  weatherIcon = `<img
-      src="http://openweathermap.org/img/wn/${weatherIcon}@2x.png"
-      id="weather-icon"
-      alt="${weatherIconDescription}"
-    />`;
-  updateValue("#current-weather-icon", weatherIcon);
 }
 
 function currentTime() {
@@ -109,11 +48,6 @@ function currentTime() {
   let curMinutes = now.getMinutes();
   formatDate(curMinutes);
 
-  // const cDate = `;
-  // ${now.getDate()}
-  // ${months[now.getMonth()]}
-  // ${now.getFullYear()}
-  // ${now.getHours()}:${now.getMinutes()}`;
   const cDate = `
   ${curDate} 
   ${months[now.getMonth()]} 
@@ -129,53 +63,47 @@ function currentTime() {
 
 currentTime();
 
-// 🕵️‍♀️Feature #2
-// Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
+function showWeather(responce) {
+  console.log(responce.data);
 
-// function updateRandomTemperature() {
-//   let temperature = Math.random();
-//   let scale = document.querySelector("#scale");
-//   temperature = temperature * 100 - 50;
-//   temperature = Math.round(temperature);
-//   document.querySelector("#temperature").innerHTML = temperature;
-// }
+  updateValue("#cityName", responce.data.name);
 
-function updateLocation(inputLocation) {
-  document.querySelector("h1").innerHTML = inputLocation;
+  celsiusTemperature = Math.round(responce.data.main.temp);
+
+  updateValue("#temperature", celsiusTemperature);
+
+  updateValue("#hymidity", responce.data.main.humidity);
+
+  updateValue("#wind", Math.round(responce.data.wind.speed));
+
+  updateValue("#clouds", responce.data.clouds.all);
+
+  weatherIcon = `<img
+      src="http://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png"
+      id="weather-icon"
+      alt="${responce.data.weather[0].description}"
+      title="${responce.data.weather[0].description}"
+    />`;
+  updateValue("#current-weather-icon", weatherIcon);
 }
 
-// In your project, when a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.
-// Please note: there's no need to include a temperature conversion at the moment. This will be taught later on in the course.
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+function search(city) {
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showWeather);
+}
 
 function changeLocation(event) {
   event.preventDefault();
   let inputLocation = document.querySelector("#search-city-input");
-  let curLocation;
+  console.log(inputLocation.value);
   if (inputLocation.value.length > 0) {
-    updateLocation(inputLocation.value);
-    curLocation = inputLocation.value;
-  } else {
-    updateLocation("Khmel'nytsky");
-    curLocation = "Khmel'nytsky";
-    alert("Please, enter a city");
+    search(inputLocation.value);
   }
-
-  const apiKey = "bd6a33bf1ae4ff07cbfe080ca9b11f66";
-  const units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${curLocation}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(changeTemperature);
-  console.log(apiUrl);
 }
 
-let searchForm = document.querySelector("#location-form");
-searchForm.addEventListener("submit", changeLocation);
-
-// 🙀 Bonus point:
-// Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.
-
-//https:api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
+// Current Geolocation weather
 function showPositionWeather(position) {
   let lon = position.coords.longitude;
   let lat = position.coords.latitude;
@@ -185,7 +113,7 @@ function showPositionWeather(position) {
   const apiKey = "bd6a33bf1ae4ff07cbfe080ca9b11f66";
   const units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(changeTemperature);
+  axios.get(apiUrl).then(showWeather);
   console.log(apiUrl);
 }
 
@@ -194,16 +122,38 @@ function currentPositionWeather(event) {
   navigator.geolocation.getCurrentPosition(showPositionWeather);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  let fTemperature = (9 * celsiusTemperature) / 5 + 32;
+  console.log(fTemperature);
+  temperatureElement.innerHTML = Math.round(fTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = celsiusTemperature;
+}
+
 let geolocationButton = document.querySelector("#current-location-button");
 geolocationButton.addEventListener("click", currentPositionWeather);
 
-function defaultTemperature() {
-  curLocation = "Kyiv";
-  const apiKey = "bd6a33bf1ae4ff07cbfe080ca9b11f66";
-  const units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${curLocation}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(changeTemperature);
-  console.log(apiUrl);
-}
+// console.log(apiUrl);
 
-defaultTemperature();
+let celsiusTemperature = null;
+
+const form = document.querySelector("#location-form");
+form.addEventListener("submit", changeLocation);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celcius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Kyiv");
